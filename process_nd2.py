57f7e2998656223d6249bd5a7ab1a96758a8706c
snkeegan <sarah@fenyolab.org>
2022-12-21 17:56:35 -0500

    Update

--- process_nd2.py
+++ process_nd2.py
@@ -1,10 +1,9 @@
 import os
-from skimage import io
-import numpy as np
 import nd2
 import glob
+from tifffile import imread, imwrite
 
-base_dir = "/Users/snk218/Dropbox/mac_files/holtlab/data_and_results/Farida_LINE1"
+base_dir = "/Users/snk218/Dropbox (NYU Langone Health)/mac_files/holtlab/data_and_results/Farida_LINE1"
 folder="20221030_LINE1"
 
 param_file=f"{base_dir}/spot_counting_parameters"
@@ -16,27 +15,10 @@ for file in glob.glob(f"{base_dir}/{folder}/*.nd2"):
     images = f.asarray()
     f.close()
 
-    tiff_stack = np.zeros(shape=[images.shape[0], images.shape[2], images.shape[3], 3], dtype=images.dtype)
-
-    for i in range(images.shape[0]):
-        cur_img = np.asarray(images[i])
-        tiff_stack[i] = np.stack([cur_img[0],cur_img[3],cur_img[1]],axis=-1)
-
-    #print(tiff_stack.shape)
-    io.imsave(f"{base_dir}/{folder}/tifs/{file_root}.tif", tiff_stack)
-    io.imsave(f"{base_dir}/{folder}/tifs/{file_root}-BF.tif", images[:,4,:,:])
+    imwrite(f"{base_dir}/{folder}/tifs/{file_root}.tif",
+            images,
+            imagej=True,
+            metadata={'axes': 'ZCYX'}
+            )
 
 
-    #spot_img = np.amax(images[:,0,:,:],0)
-    #nucl_img = np.round(np.mean(images[:,1,:,:],0)).astype(images.dtype)
-
-    #io.imsave(f"{base_dir}/{folder}/tifs/{file_root}.tif", images)
-
-    #full_img = np.stack([spot_img,np.zeros_like(spot_img),nucl_img],axis=2)
-
-    #io.imsave(f"{base_dir}/{folder}/tifs/{file_root}-ch0.tif", spot_img)
-    #io.imsave(f"{base_dir}/{folder}/tifs/{file_root}-ch1.tif", nucl_img)
-
-    #io.imsave(f"{base_dir}/{folder}/tifs/{file_root}-zproj.tif", full_img)
-
-    #f.close()
